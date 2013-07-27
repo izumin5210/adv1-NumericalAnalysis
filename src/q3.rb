f4186937538d7e3db1cc4494a84ded46651aea86
izumin <contact@izumin.info>
2013-07-28 01:11:28 +0900

    add q3

--- src/q3.rb
+++ src/q3.rb
@@ -3,29 +3,37 @@ $:.unshift File.dirname(__FILE__)
 
 require 'csv'
 
-def dft(k, upper_n, fn)
-  upper_f = Array.new
-  upper_n.times do |n|
-    upper_f.push Complex.polar(fn[n], 2*k*n*Math::PI/upper_n)
+def dft(data, n)
+  result = Array.new
+  n.times do |k|
+    result << [k, data.inject(Complex(0)) { |sum, p| sum += Complex.polar(p[1], -2*k*p[0]*Math::PI/n) }]
   end
-  upper_f
+  result
 end
 
-def power_spectrum
+def power_spectrum(data, l)
+  data.map { |p| [p[0], p[1].abs2 / (2*l)] }
 end
 
 DATA_DIR = ARGV[0] + '/q3/'
-TZ_FILE = "tf.csv"
+TZ_FILE = 'tf.csv'
+PS_FILE = 'ps.csv'
 
 OMEGA, L = 5.026548, 25.0
 N = 1024
 
 f = lambda { |t| if (0 <= t and t <= 2*L) then (Math.cos(OMEGA*t)**3)/2 else 0 end }
 
+data = Array.new
+r = Random.new(Time.now.to_i)
+N.times do |i|
+  data << [L/N*i, f.call(L/N*i)+r.rand(-1.0..1.0)]
+end
 
 CSV.open(DATA_DIR+TZ_FILE, 'w') do |csv|
-  r = Random.new(Time.now.to_i)
-  N.times do |i|
-    csv << [L/N*i, f.call(L/N*i)+r.rand(-1.0..1.0)]
-  end
+  data.each { |p| csv <<p }
+end
+
+CSV.open(DATA_DIR+PS_FILE, 'w') do |csv|
+  power_spectrum(dft(data, N), L).each { |row| csv << row }
 end
